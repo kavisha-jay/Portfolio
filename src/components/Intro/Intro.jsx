6edfbf7b34ea114b036973b4c57b1b69654d2c1f
Kavisha.me <136825573+kavisha-jay@users.noreply.github.com>
2024-03-22 01:33:43 +0530

    Major design changes in the system

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,21 +3,30 @@ import "./intro.css";
 import bg from "../../Images/me.png";
 import Animation from "../Animation/Animation";
 import {Link} from 'react-scroll';
+;
 
 import SocialButton from "../SocialMeadia/SocialButton";
 
 const Intro = () => {
   return (
   <>
+   
     <section id="intro">
+    
         <div className="introContent">
              <span className="hi">Hi,</span>
              <span className="hello">I'm <span className="fn">Kaveesha</span></span>
              <span className="itroText"><Animation /></span>
              <p className="introPara">
-              Something about me. Nothing about all.kfijqpowfoejgiewjgb.
+             I'm a passionate software engineering undergraduate with a
+             strong foundation 
+
              </p>
-              
+             <p className="introPara">
+             in computer science principles, eager to
+              translate theory into building innovative<br /> and user-friendly
+              applications.
+              </p>
              <div className="SocialBTN">
              <SocialButton/>
              </div>
@@ -27,7 +36,9 @@ const Intro = () => {
         </div>
         
         <img src={bg} alt="profile" className="bg" />
+        
     </section>
+    
   </>
   )
 };
